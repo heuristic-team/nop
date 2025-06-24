b1239fcdd46dec2728932435f073a625789c3e7a
rendemare <rendemare@gmail.com>
2025-06-24 16:22:14 +0700

    Brace elision.

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,8 +1,8 @@
 use std::iter::Peekable;
 use std::process::exit;
-use std::{ops::Index, str::CharIndices};
+use std::str::CharIndices;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Token {
     Id(String),
     Num(i64),
@@ -10,7 +10,7 @@ pub enum Token {
     ScopeEnd,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Location {
     line: usize,
     symbol: usize,
@@ -22,7 +22,7 @@ impl Location {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Span {
     start: Location,
     end: Location,
@@ -34,9 +34,9 @@ impl Span {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Lexeme {
-    token: Token,
+    pub token: Token,
     span: Span,
 }
 
@@ -50,13 +50,53 @@ type It<'a> = Peekable<CharIndices<'a>>;
 
 struct LexerCtx {
     line: usize,
+    indent: usize,
     ret: Vec<Lexeme>,
 }
 
 impl LexerCtx {
+    fn create_location(&self, tok: usize) -> Location {
+        Location::new(self.line, tok)
+    }
+
+    fn current_location<'a>(&self, it: &mut It<'a>) -> Location {
+        self.create_location(peek_location(it))
+    }
+
+    fn process_indent<'a>(&mut self, it: &mut It<'a>, indent: usize) {
+        if indent > self.indent {
+            let indent_diff = indent - self.indent;
+            for _ in 0..indent_diff {
+                let loc = self.current_location(it);
+                let span = Span::new(loc, loc);
+                let lexeme = Lexeme::new(Token::ScopeStart, span);
+                self.add_lexeme(lexeme);
+            }
+        } else if indent < self.indent {
+            let indent_diff = self.indent - indent;
+            for _ in 0..indent_diff {
+                let loc = self.current_location(it);
+                let span = Span::new(loc, loc);
+                let lexeme = Lexeme::new(Token::ScopeEnd, span);
+                self.add_lexeme(lexeme);
+            }
+        }
+        self.indent = indent;
+    }
+
+    fn close_indents(&mut self, file_size: usize) {
+        for _ in 0..self.indent {
+            let loc = Location::new(self.line, file_size);
+            let span = Span::new(loc, loc);
+            let lexeme = Lexeme::new(Token::ScopeEnd, span);
+            self.add_lexeme(lexeme);
+        }
+    }
+
     fn new() -> Self {
         LexerCtx {
             line: 0,
+            indent: 0,
             ret: Vec::new(),
         }
     }
@@ -69,70 +109,92 @@ impl LexerCtx {
     }
 }
 
-fn get_location(tok: usize, ctx: &LexerCtx) -> Location {
-    Location::new(ctx.line, tok)
+fn peek_location<'a>(it: &mut It<'a>) -> usize {
+    let loc = it.peek();
+    if let Some(&(n, _)) = loc {
+        return n;
+    } else {
+        panic!("Tried to peek location on nothing");
+    }
 }
 
-fn num<'a>(mut it: It<'a>, ctx: &mut LexerCtx) -> It<'a> {
-    let &(i, _) = it.peek().unwrap();
+fn eat_while<'a>(f: fn(char) -> bool, it: &mut It<'a>, ctx: &mut LexerCtx) -> String {
+    let mut ret = String::new();
 
-    let start_location = get_location(i, ctx);
-
-    let mut end = 0;
-    let number_string: String = it
-        .by_ref()
-        .take_while(|&(_, ch)| {
-            end += 1;
-            ch.is_numeric()
-        })
-        .map(|(_, ch)| ch)
-        .collect();
-
-    let end_location = get_location(i + end, ctx);
-
-    let span = Span::new(start_location, end_location);
-
-    let num = number_string.parse::<i64>();
-
-    let num = match num {
-        Ok(num) => num,
-        Err(error) => {
-            println!("{}", error);
-            exit(1);
+    while let Some(&(_, ch)) = it.peek() {
+        if !f(ch) {
+            break;
         }
-    };
+        ret.push(ch);
+        it.next();
+    }
 
-    let token = Token::Num(num);
+    ret
+}
+
+fn lex_number<'a>(it: &mut It<'a>, ctx: &mut LexerCtx) {
+    let start = ctx.current_location(it);
+
+    let num = eat_while(char::is_numeric, it, ctx);
+
+    let end = ctx.current_location(it);
+
+    let span = Span::new(start, end);
+
+    let token = Token::Num(num.parse().unwrap());
 
     let lexeme = Lexeme::new(token, span);
 
     ctx.add_lexeme(lexeme);
-
-    it
 }
 
-fn matcher<'a>(mut it: It<'a>, ctx: &mut LexerCtx) {
-    while let Some((_, ch)) = it.next() {
-        println!("{}", ch as u64);
+fn matcher<'a>(it: &mut It<'a>, ctx: &mut LexerCtx) {
+    lex_linestart(it, ctx);
+    while let Some(&(_, ch)) = it.peek() {
+        if ch.is_numeric() {
+            lex_number(it, ctx);
+        }
         if ch == '\n' {
-            println!("newline!");
-            ctx.new_line();
+            it.next();
+            lex_linestart(it, ctx);
             continue;
         }
         if ch.is_whitespace() {
+            it.next();
             continue;
-        }
-        if ch.is_numeric() {
-            it = num(it, ctx);
         }
     }
 }
 
+fn lex_indent<'a>(it: &mut It<'a>, ctx: &mut LexerCtx) -> usize {
+    let mut indent_amount = 0;
+    while let Some(&(_, ch)) = it.peek() {
+        if ch == '\n' {
+            return ctx.indent;
+        }
+        if !ch.is_whitespace() {
+            break;
+        }
+        indent_amount += 1;
+        it.next();
+    }
+    indent_amount / 2
+}
+
+fn lex_linestart<'a>(it: &mut It<'a>, ctx: &mut LexerCtx) {
+    ctx.new_line();
+    if !it.peek().is_none() {
+        let indent = lex_indent(it, ctx);
+        ctx.process_indent(it, indent);
+    }
+}
+
 fn perform(s: String) -> LexerCtx {
-    let iter = s.char_indices().peekable();
+    let mut iter = s.char_indices().peekable();
     let mut ctx = LexerCtx::new();
 
-    matcher(iter, &mut ctx);
+    matcher(&mut iter, &mut ctx);
+    ctx.close_indents(s.len());
 
     ctx
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use cli::get_input;
 use lexer::lex;
 
 mod cli;
+mod ir;
 mod lexer;
 
 fn main() {
     let input = get_input();
     let lexed = lex(input);
-    println!("{:?}", lexed);
+    let tokens: Vec<lexer::Token> = lexed.iter().map(|lexeme| lexeme.token.clone()).collect();
+    println!("{:?}", tokens);
 }
